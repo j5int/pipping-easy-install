96220391437e7330625dac9a998176f6dfbb284a
matth <matth@sjsoft.com>
2013-06-07 12:33:02 +0200

    Adding uninstall support.

--- pippingeasyinstall/Installer.py
+++ pippingeasyinstall/Installer.py
@@ -1,4 +1,5 @@
 import argparse
+import subprocess
 
 __author__ = 'matth'
 
@@ -14,7 +15,7 @@ import sys
 import pkg_resources
 
 def install_python_module(exe, next_count=3, wait_for_finish=120):
-    app = application.Application.start(os.path.abspath(exe))
+    app = application.Application.start('"%s"' % os.path.abspath(exe))
 
     logging.info('Waiting for Setup dialog')
     app.Setup.Wait("exists enabled visible ready", timeout=10, retry_interval=1)
@@ -37,6 +38,32 @@ def install_python_module(exe, next_count=3, wait_for_finish=120):
     logging.info('Waiting for Dialog to close')
     app.Setup.WaitNot("exists enabled visible ready", timeout=10, retry_interval=1)
 
+def uninstall_python_module(package_name):
+    install_log = os.path.abspath(os.path.join(sys.prefix, '%s-wininst.log'%package_name))
+    if not os.path.exists(install_log):
+        raise Exception('Cannot uninstall python module. Cannot find install log: %s', install_log)
+
+    uninstall_exe = os.path.abspath(os.path.join(sys.prefix, 'Remove%s.exe'%package_name))
+    if not os.path.exists(uninstall_exe):
+        raise Exception('Cannot uninstall python module. Cannot find uninstall exe: %s', uninstall_exe)
+
+    app = application.Application.start('"%s" -u "%s"' % (uninstall_exe, install_log))
+
+    logging.info('Waiting for Please confirm dialog')
+    app['Please confirm'].Wait("exists enabled visible ready", timeout=10, retry_interval=1)
+    logging.info('Waiting for Yes button')
+    app['Please confirm'].Yes.Wait("exists enabled visible ready", timeout=10, retry_interval=1)
+    app['Please confirm'].Yes.SetFocus().Click()
+
+    logging.info('Waiting for Uninstall Finished! dialog')
+    app['Uninstall Finished!'].Wait("exists enabled visible ready", timeout=120, retry_interval=1)
+    logging.info('Waiting for OK button')
+    app['Uninstall Finished!'].OK.Wait("exists enabled visible ready", timeout=10, retry_interval=1)
+    app['Uninstall Finished!'].OK.SetFocus().Click()
+
+    logging.info('Waiting for Dialog to close')
+    app['Uninstall Finished!'].WaitNot("exists enabled visible ready", timeout=10, retry_interval=1)
+
 def package_version(package):
     try:
         d = pkg_resources.get_distribution(package)
@@ -47,6 +74,7 @@ def package_version(package):
 def main():
     parser = argparse.ArgumentParser(description='Pipping Easy Install')
     parser.add_argument('packages', nargs='+', help="Stuff to install")
+    parser.add_argument('-u', '--uninstall', action='store_true', dest='uninstall', help='Uninstall instead')
     args = parser.parse_args()
 
     for p in args.packages:
@@ -54,23 +82,37 @@ def main():
 
         package_name = None
         if os.path.exists(package):
+            if args.uninstall:
+                raise ValueError('Cannot uninstall from exe: please use package name')
             exe = package
         elif package.startswith('http'):
+            if args.uninstall:
+                raise ValueError('Cannot uninstall from web url: please use package name')
             (exe, md5) = Downloader().download_file(package, os.path.basename(package).split['?'][0].split('#')[0], out=sys.stdout)
         else:
             if '==' in p:
                 (package,version) = p.split('==')
             installed_version = package_version(package)
-            if installed_version:
-                print '%s version %s already installed' % (package, installed_version)
-                continue
-            cachedir = os.environ.get('PIP_DOWNLOAD_CACHE', None)
 
-            (exe, md5) = PyPiDownloader().download_package(
-                package, version=version, out=sys.stdout, cachedir=cachedir)
-            package_name = package
+            if args.uninstall:
+                if installed_version is None:
+                    print 'Package %s is not installed' % (package)
+                    continue
 
-        if 'win' in sys.platform:
+                uninstall_python_module(package)
+
+            else:
+                if installed_version:
+                    print '%s version %s already installed' % (package, installed_version)
+                    continue
+
+                cachedir = os.environ.get('PIP_DOWNLOAD_CACHE', None)
+
+                (exe, md5) = PyPiDownloader().download_package(
+                    package, version=version, out=sys.stdout, cachedir=cachedir)
+                package_name = package
+
+        if 'win' in sys.platform and not args.uninstall:
             with RegisterPy():
                 install_python_module(exe)
             if package_name:
@@ -79,8 +121,6 @@ def main():
                     print '%s version %s now installed' % (package_name, installed_version)
                 else:
                     print "Installation completed, but package %s not installed!?" % (package_name)
-        else:
-            print 'Would execute: ' + exe
 
 if __name__ == "__main__":
     main()
